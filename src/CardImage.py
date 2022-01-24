131e8e5262d71e8f4de0712e7a19c447e67be308
Funky <piquagrearj@gmail.com>
2022-01-24 18:55:14 -0500

    Simple moving added (glitchy animation, but working). Working on stopping flipped cards from moving, then realized that my deck shuffle method isn't working right.

--- src/CardImage.py
+++ src/CardImage.py
@@ -1,17 +1,44 @@
 import os
 
+from PyQt5 import QtGui
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QLabel
 
 
-class CardImage:
-    default_image = QPixmap(os.getcwd() + "\\resources\\card_back.png")
-
-    def __init__(self, card, widget):
+class CardImage(QLabel):
+    def __init__(self, card, parent, widget):
+        super().__init__(parent)
+        # True = Back showing
+        self.flipped = True
         self.card = card
-        self.image = QLabel(widget)
-        self.image.setGeometry(0, 0, 100, 150)
-        self.image.setPixmap(QPixmap(os.getcwd() + card.__str__()))
-        print(os.getcwd() + card.__str__())
-        self.image.show()
-        # 500 x 726 Pixels
+        self.setSizePolicy(widget.sizePolicy())
+        self.setScaledContents(True)
+        self.setMinimumSize(widget.minimumSize())
+        self.setGeometry(widget.geometry())
+        self.setPixmap(widget.pixmap())
+        widget.setParent(None)
+
+        self.moving = False
+        self.position = (self.geometry().x(), self.geometry().y())
+
+    def flip(self):
+        if self.flipped:
+            self.setPixmap(QPixmap(self.card.image_location()))
+        else:
+            path = os.getcwd() + "\\resources\\card_back.png"
+            self.setPixmap(QPixmap(path))
+        self.flipped = not self.flipped
+
+    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+        if self.flipped:
+            self.moving = True
+
+    def mouseMoveEvent(self, ev: QtGui.QMouseEvent) -> None:
+        if self.moving:
+            self.move(ev.x(), ev.y())
+
+    def mouseReleaseEvent(self, ev: QtGui.QMouseEvent) -> None:
+        self.moving = False
+        self.move(self.position[0], self.position[1])
+        # TODO: If left on something that is able to, auto position it.
+        #  Else, drop it back at original position.
--- src/SolitaireSolver.py
+++ src/SolitaireSolver.py
@@ -1,9 +1,13 @@
+import array
 import sys
 
+import PyQt5
 from PyQt5 import uic
-from PyQt5.Qt import *
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from CardImage import *
+# from CardImage import CardImage
 from SolitaireDeck import *
 
 
@@ -12,13 +16,23 @@ class SolitaireSolver(QMainWindow):
         super().__init__()
         uic.loadUi('solitaire.ui', self)
 
-        piles = {self.card1: s_deck.pile1, self.card8: s_deck.pile2, self.card14: s_deck.pile3,
-                 self.card19: s_deck.pile4, self.card23: s_deck.pile5, self.card26: s_deck.pile6,
-                 self.card28: s_deck.pile7}
+        piles = {self.card1: s_deck.pile1, self.card8: s_deck.pile2,
+                 self.card14: s_deck.pile3, self.card19: s_deck.pile4,
+                 self.card23: s_deck.pile5, self.card26: s_deck.pile6,
+                 self.card28: s_deck.pile7, self.extra_card1: s_deck.extra}
         for pile in piles:
             first_card = piles[pile].__getitem__(len(piles[pile]) - 1)
             pile.setPixmap(QPixmap(first_card.image_location()))
 
+        playing_field = []
+        cards = self.playing_field.findChildren(PyQt5.QtWidgets.QLabel)
+        for i in range(len(cards)):
+            card_image = CardImage(s_deck[i], self.playing_field, cards[i])
+            playing_field.append(card_image)
+            for pile in piles:
+                first_card = piles[pile].__getitem__(len(piles[pile]) - 1)
+                if first_card.image_location() == card_image.card.image_location():
+                    card_image.flip()
         self.show()
 
 
